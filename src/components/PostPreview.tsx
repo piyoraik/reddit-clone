aba1a68be2d6ae1d7bd3afe0b7e94acf06282433
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-09-18 02:04:15 +0900

    format date

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Grid, Paper, Typography } from "@material-ui/core";
+import { Box, Grid, Paper, Typography } from "@material-ui/core";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import IconButton from "@material-ui/core/IconButton";
@@ -10,6 +10,15 @@ interface Props {
 }
 
 export default function PostPreview({ post }: Props): ReactElement {
+  const convertDateToElapsed = function (date: string): string {
+    const now = new Date(Date.now());
+    const current = new Date(date);
+
+    const diff = now.getTime() - current.getTime();
+
+    return (diff / 1000 / 60 / 60).toFixed(0);
+  };
+
   return (
     <Paper elevation={3}>
       <Grid
@@ -28,7 +37,18 @@ export default function PostPreview({ post }: Props): ReactElement {
                 <ArrowUpwardIcon />
               </IconButton>
             </Grid>
-            <Grid item>votes</Grid>
+            <Grid item>
+              <Grid container alignItems="center" direction="column">
+                <Grid item>
+                  <Typography variant="h6">
+                    {(post.upvotes - post.downvotes).toString()}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body2">votes</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
             <Grid item>
               <IconButton color="inherit" style={{ maxWidth: 20 }}>
                 <ArrowDownwardIcon />
@@ -42,7 +62,8 @@ export default function PostPreview({ post }: Props): ReactElement {
           <Grid container direction="column" alignItems="flex-start">
             <Grid item>
               <Typography variant="body1">
-                Posted by <b>{post.owner}</b> at <b>{post.createdAt}</b>
+                Posted by <b>{post.owner}</b> at{" "}
+                <b>{convertDateToElapsed(post.createdAt)} hours ago</b>
               </Typography>
             </Grid>
             <Grid item>

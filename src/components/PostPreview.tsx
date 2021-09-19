6eb4461ea4f6a4170a7cef052db85a60a4f2ec74
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-09-19 14:09:38 +0900

    update PostPreview.tsx add Image

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,9 +1,10 @@
 import React, { ReactElement } from "react";
-import { Box, Grid, Paper, Typography } from "@material-ui/core";
+import { Grid, Paper, Typography } from "@material-ui/core";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import IconButton from "@material-ui/core/IconButton";
 import { Post } from "../API";
+import Image from "next/image";
 
 interface Props {
   post: Post;
@@ -26,8 +27,9 @@ export default function PostPreview({ post }: Props): ReactElement {
         direction="row"
         justifyContent="flex-start"
         alignItems="flex-start"
+        wrap="nowrap"
         spacing={3}
-        style={{ width: "100%", padding: 12, marginTop: 24 }}
+        style={{ padding: 12, marginTop: 24 }}
       >
         {/* Upvote / votes / downvote */}
         <Grid direction="column" item spacing={1} style={{ maxWidth: 128 }}>
@@ -69,6 +71,23 @@ export default function PostPreview({ post }: Props): ReactElement {
             <Grid item>
               <Typography variant="h2">{post.title}</Typography>
             </Grid>
+            <Grid
+              item
+              style={{ maxHeight: 32, overflowY: "hidden", overflowX: "hidden" }}
+            >
+              <Typography variant="body1">{post.contents}</Typography>
+            </Grid>
+
+            {!post.image && (
+              <Grid item>
+                <Image
+                  src={"https://source.unsplash.com/random/980x540"}
+                  height={540}
+                  width={980}
+                  layout="intrinsic"
+                />
+              </Grid>
+            )}
           </Grid>
         </Grid>
       </Grid>
